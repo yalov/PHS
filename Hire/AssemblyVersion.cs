96828b7251b25665b779249cd6d0eea31d1ab2b9
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-03-02 16:40:14 -0500

    Fixed localization for Traits 	Added custom text for traits from the Community Trait Icons

--- Hire/AssemblyVersion.cs
+++ Hire/AssemblyVersion.cs
@@ -10,4 +10,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.6.8.0")]
+ [assembly: AssemblyVersion("0.6.8.1")]
--- Hire/Traits.cs
+++ Hire/Traits.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 //using CommunityTraitIcons;
 using UnityEngine;
-
+using KSP.Localization;
 using Experience;
 
 namespace Hire
@@ -19,6 +19,48 @@ namespace Hire
 
         bool Loaded = false;
 
+        string GetLocalizedCareerString(string s)
+        {
+            if (s == "Pilot")
+                return Localizer.Format("#autoLOC_500101");
+            if (s == "Engineer")
+                return Localizer.Format("#autoLOC_500103");
+            if (s == "Scientist")
+                return Localizer.Format("#autoLOC_500105");
+
+            if (s == "Unknown")
+                return Localizer.Format("#autoLOC_168872");
+            if (s == "Tourist")
+                return Localizer.Format("#autoLOC_476080");
+
+            // The following is from the Community Trait Icons mod
+            if (s == "Colonist")
+                return Localizer.Format("#TRPHire_Colonist");
+            if (s == "Geologist")
+                return Localizer.Format("#TRPHire_Geologist");
+            if (s == "Miner")
+                return Localizer.Format("#TRPHire_Miner");
+            if (s == "Mechanic")
+                return Localizer.Format("#TRPHire_Mechanic");
+            if (s == "Technician")
+                return Localizer.Format("#TRPHire_Technician");
+            if (s == "Biologist")
+                return Localizer.Format("#TRPHire_Biologist");
+            if (s == "Farmer")
+                return Localizer.Format("#TRPHire_Farmer");
+            if (s == "Medic")
+                return Localizer.Format("#TRPHire_Medic");
+            if (s == "Quartermaster")
+                return Localizer.Format("#TRPHire_Quartermaster");
+            if (s == "Kolonist")
+                return Localizer.Format("#TRPHire_Kolonist");
+            if (s == "Scout")
+                return Localizer.Format("#TRPHire_Scout");
+
+
+            return Localizer.Format(s);
+
+        }
         public Traits()
         {
             InitTraits();
@@ -26,8 +68,9 @@ namespace Hire
 
             KCareerStrings = new string[traits.Count()];
             for (int i = 0; i < traits.Count(); i++)
+            {
                 KCareerStrings[i] = traits[i];
-
+            }
 
             KCareerCnt = Math.Min(4, KCareerStrings.Count());
             KCareerGrid = new GUIContent[KCareerCnt];
@@ -38,13 +81,13 @@ namespace Hire
                 {
                     var t = CTIWrapper.CTI.getTrait(KCareerStrings[i]);
                     if (t != null)
-                        gc = new GUIContent(KCareerStrings[i], t.Icon);
+                        gc = new GUIContent(GetLocalizedCareerString(KCareerStrings[i]), t.Icon);
                     else
-                        gc = new GUIContent(KCareerStrings[i]);
+                        gc = new GUIContent(GetLocalizedCareerString(KCareerStrings[i]));
                 }
                 else
                 {
-                    gc = new GUIContent(KCareerStrings[i]);
+                    gc = new GUIContent(GetLocalizedCareerString(KCareerStrings[i]));
                 }
                 KCareerGrid[i] = gc;
             }
@@ -62,12 +105,12 @@ namespace Hire
 
                 esc.LoadTraitConfigs();
 
-           
+
                 foreach (var s in esc.TraitNames)
                 {
                     traits.Add(s);
                 }
-   
+
                 int cnt = traits.Count();
                 for (int i = 0; i < cnt; i++)
                     if (traits[i] == "Tourist" || traits[i] == "Unknown")
